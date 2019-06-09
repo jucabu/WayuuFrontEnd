9f15154b513fa6d520513ccb7e65331940c31881
Hugo Rodriguez <hrodrime@gmail.com>
2019-06-09 12:39:06 -0500

    Instalacion de ng-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -18,7 +19,8 @@ import { InfoWayuuComponent } from './views/info-wayuu/info-wayuu.component';
     InfoWayuuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
